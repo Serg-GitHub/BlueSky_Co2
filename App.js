d81abb470bb89e169de7ab8a881794897abbf029
Sergio <sergio.eneide@protonmail.com>
2021-09-14 16:14:38 +0100

    first commit

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Dearest Sara, how art thou?</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
       <Image
          
              source={{
              width:400,
-             height:500,
-             uri:"https://picsum.photos/200/300",}}/>
+             height:150,
+             uri:"https://codeclan.com/wp-content/uploads/2019/01/CodeClan-Careers-01.png",}}/>
     </SafeAreaView>
   );
 }
