843536a8100ee527e93267fecc02e84c3f3c8a5e
Simon <simonwatson176@gmail.com>
2021-09-16 10:00:42 +0100

    added transport activit to home page

--- screens/food.js
+++ screens/food.js
@@ -4,9 +4,9 @@ import { View, Button, Text } from 'react-native';
 function Food({ navigation }) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Food screen</Text>
+        <Text>Food Activity</Text>
         <Button title="Go back" onPress={() => navigation.goBack()} />
-        <Button title="Go to transport" onPress={() => navigation.navigate('Transport')} />
+        <Button title="Add a transport Activity" onPress={() => navigation.navigate('Transport')} />
 
       </View>
     );
--- screens/transport.js
+++ screens/transport.js
@@ -10,7 +10,4 @@ function Transport({ navigation }) {
       </View>
     );
   }
-
-  
-
 export default Transport;
