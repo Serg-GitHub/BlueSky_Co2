cdb486b6f073dc206d2d24c99b60eb38904b1d2b
Simon <simonwatson176@gmail.com>
2021-09-18 17:16:05 +0100

    added input boxes

--- components/foodComponents/chickenComponent.js
+++ components/foodComponents/chickenComponent.js
@@ -1,13 +1,33 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 
 
 const chickenComponent=()=>{
+         const [number, onChangeNumber] = React.useState(null);
+
     return(
         <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
-        <Text>im a chicken</Text>
-        </View>
+        <Text>How many kilograms of Chicken do you eat a week?</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Enter number"
+        keyboardType="numeric"
+     
+      />
+      <Button title = "Add Activity"/>
+        
+      </View>
     )   
     
 }
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
 export default chickenComponent
