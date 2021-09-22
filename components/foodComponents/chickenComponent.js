ee19b1705acc9769aa9e282bd9e67a01d8b615e5
Sergio <sergio.eneide@protonmail.com>
2021-09-22 17:22:35 +0100

    styling on food components

--- components/foodComponents/chickenComponent.js
+++ components/foodComponents/chickenComponent.js
@@ -30,12 +30,5 @@ const chickenComponent=({navigation})=>{
     )   
     
 }
-// const styles = StyleSheet.create({
-//     input: {
-//       height: 40,
-//       margin: 12,
-//       borderWidth: 1,
-//       padding: 10,
-//     },
-//   });
+
 export default chickenComponent
